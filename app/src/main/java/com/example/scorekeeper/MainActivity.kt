df3f6ac2424f248135e6d96b38cb697c57dcc031
99prabh <133125732+99prabh@users.noreply.github.com>
2023-07-05 10:36:14 -0400

    Comments added for MainActivity.kt

--- app/src/main/java/com/example/scorekeeper/MainActivity.kt
+++ app/src/main/java/com/example/scorekeeper/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.scorekeeper
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var team1ScoreTextView: TextView
     private lateinit var team2ScoreTextView: TextView
     private lateinit var team1Spinner: Spinner
@@ -18,11 +19,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize views
         team1ScoreTextView = findViewById(R.id.team1Score)
         team2ScoreTextView = findViewById(R.id.team2Score)
         team1Spinner = findViewById(R.id.scoringSpinnerTeam1)
         team2Spinner = findViewById(R.id.scoringSpinnerTeam2)
 
+        // Set up the scoring options in the spinner
         val scoringOptions = listOf("1", "2", "3", "6")
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, scoringOptions)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -30,54 +34,73 @@ class MainActivity : AppCompatActivity() {
         team1Spinner.adapter = adapter
         team2Spinner.adapter = adapter
 
+        // Handle the click events for the buttons
         val team1IncreaseButton = findViewById<Button>(R.id.team1IncreaseButton)
         val team1DecreaseButton = findViewById<Button>(R.id.team1DecreaseButton)
         val team2IncreaseButton = findViewById<Button>(R.id.team2IncreaseButton)
         val team2DecreaseButton = findViewById<Button>(R.id.team2DecreaseButton)
 
+        // Increase Team 1 score when increase button is clicked
         team1IncreaseButton.setOnClickListener {
             val score = team1Spinner.selectedItem.toString().toInt()
             increaseTeam1Score(score)
         }
+
+        // Decrease Team 1 score when decrease button is clicked
         team1DecreaseButton.setOnClickListener {
             val score = team1Spinner.selectedItem.toString().toInt()
             decreaseTeam1Score(score)
         }
+
+        // Increase Team 2 score when increase button is clicked
         team2IncreaseButton.setOnClickListener {
             val score = team2Spinner.selectedItem.toString().toInt()
             increaseTeam2Score(score)
         }
 
+        // Decrease Team 2 score when decrease button is clicked
         team2DecreaseButton.setOnClickListener {
             val score = team2Spinner.selectedItem.toString().toInt()
             decreaseTeam2Score(score)
         }
     }
+
+    // Increase Team 1 score by the given score value
     private fun increaseTeam1Score(score: Int) {
         team1Score += score
         updateTeam1ScoreText()
     }
+
+    // Decrease Team 1 score by the given score value (if possible)
     private fun decreaseTeam1Score(score: Int) {
         if (team1Score >= score) {
             team1Score -= score
             updateTeam1ScoreText()
         }
+        // Add handling for negative scores or score limits if required
     }
 
+    // Increase Team 2 score by the given score value
     private fun increaseTeam2Score(score: Int) {
         team2Score += score
         updateTeam2ScoreText()
     }
+
+    // Decrease Team 2 score by the given score value (if possible)
     private fun decreaseTeam2Score(score: Int) {
         if (team2Score >= score) {
             team2Score -= score
             updateTeam2ScoreText()
         }
+        // Add handling for negative scores or score limits if required
     }
+
+    // Update the displayed Team 1 score on the UI
     private fun updateTeam1ScoreText() {
         team1ScoreTextView.text = team1Score.toString()
     }
 
+    // Update the displayed Team 2 score on the UI
     private fun updateTeam2ScoreText() {
         team2ScoreTextView.text = team2Score.toString()
     }
