b33f0789209d0279fe77a15632d03903fa9f1277
99prabh <133125732+99prabh@users.noreply.github.com>
2023-07-05 10:22:13 -0400

    Added variables and setting view values in MainActivity.kt

--- app/src/main/java/com/example/scorekeeper/MainActivity.kt
+++ app/src/main/java/com/example/scorekeeper/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.scorekeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var team1ScoreTextView: TextView
+    private lateinit var team2ScoreTextView: TextView
+    private lateinit var team1Spinner: Spinner
+    private lateinit var team2Spinner: Spinner
+    private var team1Score = 0
+    private var team2Score = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        team1ScoreTextView = findViewById(R.id.team1Score)
+        team2ScoreTextView = findViewById(R.id.team2Score)
+        team1Spinner = findViewById(R.id.scoringSpinnerTeam1)
+        team2Spinner = findViewById(R.id.scoringSpinnerTeam2)
+
     }
 }
