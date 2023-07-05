ee488eba2f34431b83a8fb882242b2d559a60ac9
99prabh <133125732+99prabh@users.noreply.github.com>
2023-07-05 10:26:00 -0400

    Added scoring options and button variables MainActivity.kt

--- app/src/main/java/com/example/scorekeeper/MainActivity.kt
+++ app/src/main/java/com/example/scorekeeper/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.scorekeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 import android.widget.TextView
 
@@ -20,6 +22,18 @@ class MainActivity : AppCompatActivity() {
         team2ScoreTextView = findViewById(R.id.team2Score)
         team1Spinner = findViewById(R.id.scoringSpinnerTeam1)
         team2Spinner = findViewById(R.id.scoringSpinnerTeam2)
+
+        val scoringOptions = listOf("1", "2", "3", "6")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, scoringOptions)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        team1Spinner.adapter = adapter
+        team2Spinner.adapter = adapter
+
+        val team1IncreaseButton = findViewById<Button>(R.id.team1IncreaseButton)
+        val team1DecreaseButton = findViewById<Button>(R.id.team1DecreaseButton)
+        val team2IncreaseButton = findViewById<Button>(R.id.team2IncreaseButton)
+        val team2DecreaseButton = findViewById<Button>(R.id.team2DecreaseButton)
 
     }
 }
